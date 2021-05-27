0cdce0b3d90f33fc6302c8855d751e1f921c6df4
Taqi Aziz <taqi.aziz@tech.jago.com>
2021-05-28 02:14:46 +0700

    sandboxing: enhance get env method

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,5 @@ COPY . ./
 
 ENV RUNNER_ALLOW_RUNASROOT=1
 
+CMD [ "sh", "setup_env.sh" ]
 ENTRYPOINT ["sh", "linux_arm_setup.sh"]
--- linux_arm_setup.sh
+++ linux_arm_setup.sh
@@ -1,5 +1,5 @@
-echo runner...
-mkdir actions-runner && cd actions-runner
+mkdir actions-runner
+cd actions-runner
 curl -o actions-runner-linux-x64-2.278.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-x64-2.278.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.278.0.tar.gz
 ./config.sh --url $_REPO_PATH --token $_GITHUB_TOKEN
